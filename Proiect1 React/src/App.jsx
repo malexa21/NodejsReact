d8fbe5d0d57f96d2fb17eadd9db0129f2ad9b935
malexa21 <aaliroalexandra@gmail.com>
2023-09-07 16:50:42 +0300

    Am adaugat texte

--- Proiect1 React/src/App.jsx
+++ Proiect1 React/src/App.jsx
@@ -1,19 +1,36 @@
 const Salut = () =>{
-  return (<h1>salutare</h1>)
+  return (<h1>Salutare</h1>)
 }
 
 
 const Maestrul = () =>{
-  return(<img src="marius.jpg"/>)
+  return(<img src="marius.jpg" id="marius"/>)
 }
+
+const IntroducereMarius = () =>{
+  return(<div>
+    <h3>El este maestrul Marius maimuta</h3>
+    <h3>Marius este un maimutoi foarte inteligent</h3>
+  </div>
+  )
+}
+
+
+
 
 
 const App = () => {
  return ( <div>
+  <center>
     <Salut/>
     <Maestrul/>
+    <IntroducereMarius/>
+    </center>
  </div>
  );
 };
+
+
+
 
 export default App
