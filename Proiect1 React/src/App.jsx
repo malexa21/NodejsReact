208cf8a489e1c8024ae3c7632c19172110d764f4
malexa21 <aaliroalexandra@gmail.com>
2023-09-07 17:30:40 +0300

    Am facut ultimele modificari

--- Proiect1 React/src/App.jsx
+++ Proiect1 React/src/App.jsx
@@ -2,21 +2,23 @@ const Salut = () =>{
   return (<h1>Salutare</h1>)
 }
 
-
 const Maestrul = () =>{
   return(<img src="marius.jpg" id="marius"/>)
 }
 
 const IntroducereMarius = () =>{
   return(<div>
-    <h3>El este maestrul Marius maimuta</h3>
+    <h3>El este maestrul Marius</h3>
     <h3>Marius este un maimutoi foarte inteligent</h3>
   </div>
   )
 }
 
+const NumarNorocos = numar =>{
+  const {nrAles} = numar;
 
-
+  return(<h1>Marius iti daruieste numarul lui de telefon {nrAles}</h1>)
+}
 
 
 const App = () => {
@@ -25,6 +27,7 @@ const App = () => {
     <Salut/>
     <Maestrul/>
     <IntroducereMarius/>
+    <NumarNorocos nrAles={"23456787643"}/>
     </center>
  </div>
  );
