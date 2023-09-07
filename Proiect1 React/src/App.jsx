6a2349b5cb74c82fcebc0bbed3e6d8a774feefd2
malexa21 <aaliroalexandra@gmail.com>
2023-09-07 15:47:27 +0300

    L-am adaugat pe marius

--- Proiect1 React/src/App.jsx
+++ Proiect1 React/src/App.jsx
@@ -1,12 +1,17 @@
 const Salut = () =>{
-  return <h1>salutare</h1>;
+  return (<h1>salutare</h1>)
 }
 
+
+const Maestrul = () =>{
+  return(<img src="marius.jpg"/>)
+}
 
 
 const App = () => {
  return ( <div>
     <Salut/>
+    <Maestrul/>
  </div>
  );
 };
